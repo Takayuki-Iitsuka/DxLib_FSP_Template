2cf77699ef30047df0fab3ab5a37051f039261ec
takayuki.iitsuka <takayuki.iitsuka@gmail.com>
2023-06-20 13:53:19 +0900

    コメント追加

--- DxLib_FSP_Template.cpp
+++ DxLib_FSP_Template.cpp
@@ -1,13 +1,20 @@
+//DxLib_FSP_Template.cpp
 //
 //#include <windows.h>
 #include <DxLib.h>
 #include "fps.h"
 #include <cmath>
 
-//
-#define W_Title "DxLib Test..."
+// マクロ定義
+#define W_Title "DxLib Test..." //　タイトル
 #define W_Vsync TRUE // V-Syncを使うならTRUE
-#define W_Icon 555
+#define W_Icon 555 //　アイコン
+
+const double MY_PI = 3.141592653589793238462643;
+
+const int W_Width = 1280; //　画面の幅
+const int W_Height = 720; // 画面の高さ
+const int W_ColorBit = 32; // 画面の色
 
 // プロトタイプ宣言
 int WINAPI WinMain(_In_	HINSTANCE, _In_opt_ HINSTANCE,
@@ -18,68 +25,80 @@ int WINAPI WinMain(_In_	HINSTANCE hInstance, _In_opt_ HINSTANCE	hPrevInstance,
 				   _In_	LPSTR lpCmdLine, // LPSTR or PSTR
 				   _In_	int	nCmdShow)
 {
+	// Log.txtの出力
 #ifndef _DEBUG // ifndef or
 	SetOutApplicationSystemLogValidFlag(FALSE); // log.txt
 #endif
 	//
-	const int W_Width = 1280;
-	const int W_Height = 720;
-	const int W_ColorBit = 32;
-	const double MY_PI = 3.141592653589793238462643;
 
-	ChangeWindowMode(TRUE);
-	SetGraphMode(W_Width, W_Height, W_ColorBit);
+	ChangeWindowMode(TRUE); //ウィンドウモードの設定
+	SetGraphMode(W_Width, W_Height, W_ColorBit); //解像度の設定
 	SetWindowSizeChangeEnableFlag(TRUE);
-	//SetWindowSize(W_Width, W_Height);
-	SetMainWindowText(W_Title);
-	SetBackgroundColor(10, 90, 150);
-	SetWaitVSyncFlag(W_Vsync);
-	SetAlwaysRunFlag(TRUE);
+	//SetWindowSize(W_Width, W_Height); //ウィンドウの大きさ設定
+	SetMainWindowText(W_Title); //ウィンドウのタイトル設定
+	SetBackgroundColor(10, 90, 150); //ウィンドウの背景色
+	SetWaitVSyncFlag(W_Vsync); //垂直同期の設定
+	SetAlwaysRunFlag(TRUE); //非アクティブでも実行する
 	SetWindowIconID(W_Icon); // アイコン
 
+	//DxLibの初期化
 	if (DxLib_Init() == -1)
 	{
-		return -1;
+		return -1; //DxLib初期化失敗ならソフト終了
 		//return EXIT_FAILURE; // 1 or -1
 	}
 
+	//裏画面に描画する（ダブルバッファリング）
 	SetDrawScreen(DX_SCREEN_BACK);
 
-	//FPS
+	//FPSの初期化
 	FPSInit();
 
+	//無限ループ（ゲームループ）
 	while (TRUE)
 	{
+		//メッセージ処理をする（マウス操作やキー入力などを受け続ける）
 		if (ProcessMessage() != 0)
 		{
-			break;
+			break; //無限ループから出る（ゲーム終了）
 		}
+		//画面を消去する（１ループずつ書き換える）
 		if (ClearDrawScreen() != 0)
 		{
-			break;
+			break; //無限ループから出る（ゲーム終了）
 		}
-		// main loop
-		{
-			FPSCheck();
-			FPSDraw();
-			FPSWait();
 
+		// main loop
+		//ここに全ての処理動作が入る
+		{
+			FPSCheck(); //FPS計測
+			////////////////////////////
+
+			
 			DrawString(50, 50, "Hello...! DxLib.......!", GetColor(250, 190, 255));
 			DrawString((W_Width / 2) - 150, (W_Height / 2) - 60, "Takayuki Iitsuka", GetColor(250, 190, 255));
 			DrawString((W_Width / 2) - 150, W_Height / 2, "飯塚　隆行　いいつか　たかゆき　ｲｲﾂｶ ﾀｶﾕｷ", GetColor(250, 190, 255));
 
 			DrawPixel(320, 240, GetColor(255, 255, 255));
 
+
+			////////////////////////////
+			FPSDraw(); //FPS描画
+			FPSWait(); //FPS待機
 		}
-		//
+		//main loop End
+		//ここまで、全ての処理動作が入る
+
+		//裏画面を画面に描画
 		ScreenFlip();
 
 	} // main loop
 
-	//WaitKey();
-	DxLib_End();
-	//return 0;
-	return EXIT_SUCCESS;
+	//WaitKey(); //キー入力待機
+
+	DxLib_End(); //DxLib終了
+	
+	return EXIT_SUCCESS; // ソフト終了 return 0;
 }
 
 // End
--- fps.h
+++ fps.h
@@ -3,23 +3,27 @@
 
 #include <DxLib.h>
 
-#define W_FPS			60
-#define MicroSecond		1000000.0f
-#define MillSecond		1000.0f
-#define WaitTimeMill	3000
+//マクロ定義
+#define W_FPS			60 //FPSの設定
+#define MicroSecond		1000000.0f //1マクロ秒
+#define MillSecond		1000.0f //1ミリ秒
+#define WaitTimeMill	3000 //最大で待てるミリ秒
 
+//構造体の定義
 typedef struct _FPS {
-	LONGLONG FirstTakeTime = 0;
-	LONGLONG NowTakeTime = 0;
-	LONGLONG OldTakeTime = 0;
+	LONGLONG FirstTakeTime = 0; //１フレーム目の計測時間
+	LONGLONG NowTakeTime = 0; //現在の計測時間
+	LONGLONG OldTakeTime = 0; //以前の計測時間
 
-	float DeltaTime = 0.000001f;
-	int FrameCount = 1;
-	float Average = 0.0f;
+	float DeltaTime = 0.000001f; //デルタタイム（経過時間）
+	int FrameCount = 1; //現在のフレーム数（１フレーム目～MAXフレーム目まで）
+	float Average = 0.0f; //平均FPS値
 }FPS;
 
+//外部グローバル変数
 extern FPS fps;
 
+//外部プロトタイプ宣言
 extern VOID FPSInit(VOID);
 extern VOID FPSCheck(VOID);
 extern VOID FPSDraw(VOID);
